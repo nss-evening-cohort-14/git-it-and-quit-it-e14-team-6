d6e69227a4fbf932772ba087064984103443b713
fimoefive <brothermj@hotmail.com>
2021-02-09 16:18:37 -0600

    added link and script tags to all the html files and HTML Boiler plates to all HTML files. Also added Nav bar and links to all the HTML files with a header at the top of every HTML file. Added a data:image/x-icon link in the Boiler plate of all HTML files to block a console ERROR code from running. I wrote and setup all the fundamental JS functions that we would be using for this application.

--- main.js
+++ main.js
@@ -1,1 +1,62 @@
 console.log("CONNECTED");
+
+// Repositories Array
+const repositories = [];
+// Deleted Repositories Array
+const deletedRepositories = [];
+
+// Packages Array
+const packages = [];
+// Deleted Packages Array
+const deletedPackages = [];
+
+// Projects Array
+const projects = [];
+// Deleted Projects Array
+const deletedProjects = [];
+
+// Print to DOM function
+const printToDom = () => {
+
+};
+
+// Card Information Form
+const cardInfo = function (event) {
+  event.preventDefault();
+
+};
+
+// Card Input Information Form
+const cardForm = function () {
+  let domString = "";
+
+};
+
+
+
+
+
+
+
+
+
+// Delete Card Function
+const deleteCard = () => {
+
+};
+
+// Click Event Function that listens to the buttons (onClick)
+const clickEvents = function () {
+  document.querySelector("").addEventListener("click", deleteCard);
+  document.querySelector("").addEventListener("click",);
+  document.querySelector("").addEventListener("click",);
+  document.querySelector("").addEventListener("click",);
+};
+
+
+// Initializes all other Functions to run
+const initialize = () => {
+  clickEvents();
+};
+// Invokes the Initialize Function
+// initialize();
